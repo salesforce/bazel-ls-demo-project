7e7066dba2a59e936522d6408ee4f6d862b4ec7b
Gunnar Wagenknecht <gwagenknecht@salesforce.com>
2020-02-19 11:22:28 +0100

    Initial commit

---
+++ module1/java/src/hello/Hello.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Hello {
+
+
+    public static void main(String[] args) {
+        System.out.println(new mybuilder_sample_generation.MybuilderSampleGeneration());
+    }
+}
