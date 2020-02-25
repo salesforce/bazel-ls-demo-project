a2cf84fdc28538124474d39035b9ba568a398773
Siarhei Tsitou <siarhei_tsitou@epam.com>
2020-02-25 16:54:15 +0300

    add dependent module2; add maven dep usage

--- module1/java/src/hello/Hello.java
+++ module1/java/src/hello/Hello.java
@@ -1,9 +1,23 @@
 package hello;
 
+import library.Greeting;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Iterables;
+
 public class Hello {
 
 
     public static void main(String[] args) {
+    	Greeting greeter = new Greeting();
         System.out.println(new mybuilder_sample_generation.MybuilderSampleGeneration());
+        
+        List<String> modules = Lists.newArrayList("module1", "module2");
+
+        Iterable<String> result = Iterables.transform(modules, greeter::greet);
+        
+        result.forEach(System.out::println);
     }
 }
